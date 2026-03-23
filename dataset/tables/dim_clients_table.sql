ac8d9d2c21a573ab633b35f366821867854b1c27
Mpho <matebesimoe89@gmail.com>
2026-03-23 22:01:26 +0200

    created a dim clients table

---
+++ dataset/tables/dim_clients_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Dim_Clients (
+	ClientsID INT PRIMARY KEY,
+	ClientName VARCHAR (255),
+	City VARCHAR (255),
+	Province VARCHAR (255),
+	Country VARCHAR (255),
+	Continent VARCHAR (255)
+);
