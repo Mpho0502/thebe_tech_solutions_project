74d528afc84d15f6dcbfe9f5a6646464bc5e3e05
Mpho <matebesimoe89@gmail.com>
2026-03-23 23:46:00 +0200

    inserted 10 values, 11-20, into the dim clients table

--- dataset/tables/dim_clients_table.sql
+++ dataset/tables/dim_clients_table.sql
@@ -17,5 +17,15 @@ INSERT INTO Dim_Clients VALUES
 (7, 'Theta Group', 'Accra', 'Greater Accra', 'Ghana', 'Africa'),
 (8, 'Iota Partners', 'Cairo', 'Cairo Governorate', 'Egypt', 'Africa'),
 (9, 'Kappa Logistics', 'Casablanca', 'Casablanca-Settat', 'Morocco', 'Africa'),
-(10, 'Lambda Media', 'Addis Ababa', 'Addis Ababa', 'Ethiopia', 'Africa')
+(10, 'Lambda Media', 'Addis Ababa', 'Addis Ababa', 'Ethiopia', 'Africa'),
+(11, 'London Legacy', 'London', 'Greater London', 'United Kingdom', 'Europe'),
+(12, 'Madrid Merit', 'Madrid', 'Community of Madrid', 'Spain', 'Europe'),
+(13, 'Rome Radiance', 'Rome', 'Lazio', 'Italy', 'Europe'),
+(14, 'Amsterdam Ally', 'Amsterdam', 'North Holland', 'Netherlands', 'Europe'),
+(15, 'Brussels Bridge', 'Brussels', 'Brussels-Capital', 'Belgium', 'Europe'),
+(16, 'Vienna Valour', 'Vienna', 'Vienna', 'Austria', 'Europe'),
+(17, 'Stockholm Star', 'Stockholm', 'Stockholm County', 'Sweden', 'Europe'),
+(18, 'Zurich Zenith', 'Zurich', 'Canton of Zurich', 'Switzerland', 'Europe'),
+(19, 'Oslo Ocean', 'Oslo', 'Oslo County', 'Norway', 'Europe'),
+(20, 'Dublin Draft', 'Dublin', 'Leinster', 'Ireland', 'Europe')
 ;
