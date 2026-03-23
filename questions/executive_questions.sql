1aeb1b11c74ff0c3791e67b3b5468f4c52acc2ce
Mpho <matebesimoe89@gmail.com>
2026-03-24 01:30:28 +0200

    adding executive questions and peformance analysis scripts

---
+++ questions/executive_questions.sql
@@ -0,0 +1,24 @@
+--Executive Questions From Board Meeting
+
+
+--1. Which employees are listed in the system?
+--2. How many employees do we have in total? 
+--3. Which clients are registered in our database?  
+--4. How many clients are based in South Africa?
+--5. List all accounts and their types.
+--6. How many products fall under the ‘Security’ category?  
+--7. Show all sales transactions recorded in the fact table.  
+--8. How many sales transactions occurred in 2023?
+--9. Which employees generated the highest number of sales transactions?  
+--10. What is the total revenue (TotalPrice) per year?
+--11. What are the total profits per quarter across all years?  
+--12. Which product categories generated the most revenue?  
+--13. Which clients contributed the highest profits?
+--14. How many transactions involved discounts greater than 100?  
+--15. List all employees with their job titles and total sales revenue.
+--16. Which accounts generated the most sales transactions?  
+--17. Show total expenses per year.
+--18. Which month had the highest sales revenue in 2022?
+--19. List all products sold in 2024 with their total quantities.  
+--20. Which continent contributed the most revenue overall?  
+   
