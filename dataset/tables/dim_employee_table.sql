a360fcb45e6afcc0ec4ab0a8385d5330518981c5
Mpho <matebesimoe89@gmail.com>
2026-03-23 23:30:23 +0200

    inserted 10 more values into the dim employee table

--- dataset/tables/dim_employee_table.sql
+++ dataset/tables/dim_employee_table.sql
@@ -16,5 +16,15 @@ INSERT INTO Dim_Emloyee VALUES
 (7, 'Richard','Taylor','richard.taylor5@thebe.com','Product Owner'),
 (8, 'James','Whayne','james.whayne@thebe.com','Tech Consultant'),
 (9, 'Thomas','Martin','thomas.martin9@thebe.com','HR Specialist'),
-(10, 'Elizabeth','Lopez','elizabeth.lopez10@thebe.com','Senior Engineer')
+(10, 'Elizabeth','Lopez','elizabeth.lopez10@thebe.com','Senior Engineer'),
+(11, 'Nancy', 'Smith', 'nancy.smith11@thebe.com', 'Data Analyst'),
+(12, 'Thomas', 'Smith', 'thomas.smith12@thebe.com', 'Sales Manager'),
+(13, 'James', 'Lopez', 'james.lopez13@thebe.com', 'Tech Consultant'),
+(14, 'Sarah', 'Davis', 'sarah.davis14@thebe.com', 'Data Analyst'),
+(15, 'James', 'Williams', 'james.williams15@thebe.com', 'HR Specialist'),
+(16, 'William', 'Hernandez', 'william.hernandez16@thebe.com', 'Account Executive'),
+(17, 'Susan', 'Moore', 'susan.moore17@thebe.com', 'Data Analyst'),
+(18, 'Karen', 'Johnson', 'karen.johnson18@thebe.com', 'Marketing Coordinator'),
+(19, 'Sarah', 'Taylor', 'sarah.taylor19@thebe.com', 'Tech Consultant'),
+(20, 'Michael', 'Martinez', 'michael.martinez20@thebe.com', 'Account Executive')
 ;
