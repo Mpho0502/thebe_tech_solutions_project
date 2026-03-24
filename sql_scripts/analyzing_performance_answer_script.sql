2be82887d8040d4e52748bfd25959cec8f658a57
Mpho <matebesimoe89@gmail.com>
2026-03-24 13:54:49 +0200

    answers from 11-15 on the answer script

--- sql_scripts/analyzing_performance_answer_script.sql
+++ sql_scripts/analyzing_performance_answer_script.sql
@@ -67,10 +67,53 @@ ON f.DateID = d.DateID
 group by d.Year;
 
 --11. What are the total profits per quarter across all years?  
+
+SELECT d.Year, d.Quarter,
+	SUM(f.TotalProfits) as Total_profits
+FROM Thebe_Tech_Solutions_Sales_Fact AS f
+INNER JOIN Dim_Date AS d
+ON f.DateID = d.DateID
+GROUP BY d.Year, d.Quarter;
+
 --12. Which product categories generated the most revenue?  
+
+SELECT p.Category,
+	SUM(f.TotalPrice) AS Revenue
+FROM Thebe_Tech_Solutions_Sales_Fact AS f
+INNER JOIN Dim_Product AS p
+ON f.ProductID = p.ProductID
+GROUP BY p.Category
+ORDER by Revenue DESC;
+
 --13. Which clients contributed the highest profits?
+
+SELECT TOP 10 c.ClientName, 
+	SUM(f.TotalProfits) AS Profits
+FROM [Thebe_Tech_Solutions_Sales_Fact] AS f
+INNER JOIN Dim_Clients AS c
+ON f.ClientsID = c.ClientsID
+GROUP BY c.ClientName
+ORDER BY Profits DESC;
+
 --14. How many transactions involved discounts greater than 100?  
+
+SELECT
+	COUNT(*) AS Discounts
+FROM Thebe_Tech_Solutions_Sales_Fact
+WHERE Discounts > 100;
+
 --15. List all employees with their job titles and total sales revenue.
+
+SELECT e.Name,
+	   e.Surname,
+	   e.JobTitle,
+	SUM(f.TotalPrice) AS Revenue
+FROM Thebe_Tech_Solutions_Sales_Fact AS f
+INNER JOIN Dim_Emloyee AS e
+ON f.EmployeeID = e.EmployeeID
+GROUP BY e.Name, e.Surname, e.JobTitle
+ORDER BY Revenue DESC;
+
 --16. Which accounts generated the most sales transactions?  
 --17. Show total expenses per year.
 --18. Which month had the highest sales revenue in 2022?
